7f13c02845aac27e7e2bfb3c3860a4e9244d6283
agathaamalia <agathahusnaamalia2004@mail.ugm.ac.id>
2025-09-12 21:48:17 +0700

    Add Route, Schedule, and Ship classes

---
+++ Route.cs
@@ -0,0 +1,11 @@
+﻿namespace NaviGo_CD
+{
+    public class Route
+    {
+        public string RouteId { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string Duration { get; set; }
+    }
+}
+
